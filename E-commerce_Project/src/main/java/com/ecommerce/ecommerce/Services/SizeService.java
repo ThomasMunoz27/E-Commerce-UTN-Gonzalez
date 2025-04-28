dc8feeef6ae4d754c771c90e0dcb9843a8fa61fe
erredev-JS <roddev100@gmail.com>
2025-04-28 19:28:32 -0300

    Fix

--- E-commerce_Project/src/main/java/com/ecommerce/ecommerce/Services/SizeService.java
+++ E-commerce_Project/src/main/java/com/ecommerce/ecommerce/Services/SizeService.java
@@ -4,7 +4,9 @@ import com.ecommerce.ecommerce.Entities.Size;
 import com.ecommerce.ecommerce.Repositories.SizeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Service;
 
+@Service
 public class SizeService extends BaseService<Size> {
 
     @Autowired
