d76ce470d87b58cb08a5f310e22350aad629aa94
ThomasMunoz27 <thomasnm2004@gmail.com>
2025-05-05 22:13:37 -0300

    bill detail controller

---
+++ E-commerce_Project/src/main/java/com/ecommerce/ecommerce/Controllers/BillDetailController.java
@@ -0,0 +1,15 @@
+package com.ecommerce.ecommerce.Controllers;
+
+import com.ecommerce.ecommerce.Entities.BillDetail;
+import com.ecommerce.ecommerce.Services.BaseService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/bill-detail")
+public class BillDetailController extends BaseController<BillDetail>{
+
+    public BillDetailController(BaseService<BillDetail> baseService) {
+        super(baseService);
+    }
+}
