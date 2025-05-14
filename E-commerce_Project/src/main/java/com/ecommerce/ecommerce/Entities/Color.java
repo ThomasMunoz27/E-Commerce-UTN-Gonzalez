0072d260409a10eab75942be4051b90833d4acfa
maurogarzia <maurogarzia2@gmail.com>
2025-05-14 12:49:01 -0300

    I change the name to the value in entity Color

--- E-commerce_Project/src/main/java/com/ecommerce/ecommerce/Entities/Color.java
+++ E-commerce_Project/src/main/java/com/ecommerce/ecommerce/Entities/Color.java
@@ -14,6 +14,6 @@ import lombok.Setter;
 @Setter
 public class Color extends Base{
 
-    @Column(name = "nombre")
-    private String name;
+    @Column(name = "valor")
+    private String value;
 }
