c40ded55e2e48f37035fb51dba5e050ff32d1c9c
ThomasMunoz27 <thomasnm2004@gmail.com>
2025-06-09 22:38:45 -0300

    fix cloudinary

--- E-commerce_Project/src/main/java/com/ecommerce/ecommerce/Controllers/UploadController.java
+++ E-commerce_Project/src/main/java/com/ecommerce/ecommerce/Controllers/UploadController.java
@@ -6,12 +6,11 @@ import com.ecommerce.ecommerce.Services.CloudinaryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-@Controller
+@RestController
+@CrossOrigin("*")
 @RequestMapping("/api/upload")
 public class UploadController {
     @Autowired
